20f6a35efee21d193784882e565b1fa4eec9f315
Armagaiden <38038560+Armagaiden@users.noreply.github.com>
2021-04-30 07:36:32 +0200

    Add files via upload

---
+++ TP05/Models/PizzaViewModel.cs
@@ -0,0 +1,21 @@
+﻿using TP05.ValidationAttributes;
+using PizzaClassLibrary.Entities;
+using PizzaClassLibrary.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TP05.Models
+{
+    public class PizzaViewModel
+    {
+        public Pizza Pizza { get; set; }
+        public List<Pate> Pates { get; set; }
+        public List<SelectListItem> Ingredients { get; set; }
+
+        [ListRange(Max = 5, Min = 2)]
+        public List<int> IngredientIds { get; set; }
+    }
+}
